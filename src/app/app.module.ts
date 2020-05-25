b8b8d689b7a3758d5b691e20c8089f1021de7861
thisissomnath <sonawanesomanath543@gmail.com>
2020-05-25 21:55:43 +0530

    newcreate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TotalCountComponent } from './component/dashbord/total-count/total-count.component';
+import { UserDashboardComponent } from './component/Dashboard/user-dashboard/user-dashboard.component';
+import { CovidGraphComponent } from './component/Links/covid-graph/covid-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TotalCountComponent,
+    UserDashboardComponent,
+    CovidGraphComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/Links/covid-graph/covid-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-covid-graph',
+  templateUrl: './covid-graph.component.html',
+  styleUrls: ['./covid-graph.component.css']
+})
+export class CovidGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
